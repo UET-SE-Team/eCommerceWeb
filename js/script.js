4a7614d9a8a9d26d765ec96bbe785035dd6623ab
tlam25 <126168867+tlam25@users.noreply.github.com>
2024-03-31 00:22:05 +0700

    [#2] Decor more for login box.

--- js/script.js
+++ js/script.js
@@ -1,39 +1,48 @@
 // init Isotope
 var $grid = $('.collection-list').isotope({
-    // options
-  });
-  // filter items on button click
-  $('.filter-button-group').on( 'click', 'button', function() {
-    var filterValue = $(this).attr('data-filter');
-    resetFilterBtns();
-    $(this).addClass('active-filter-btn');
-    $grid.isotope({ filter: filterValue });
-  });
+  // options
+});
+// filter items on button click
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  resetFilterBtns();
+  $(this).addClass('active-filter-btn');
+  $grid.isotope({ filter: filterValue });
+});
 
-  var filterBtns = $('.filter-button-group').find('button');
-  function resetFilterBtns() {
-    filterBtns.each(function() {
-        $(this).removeClass('active-filter-btn');
-    });
-  }
+var filterBtns = $('.filter-button-group').find('button');
+function resetFilterBtns() {
+  filterBtns.each(function() {
+      $(this).removeClass('active-filter-btn');
+  });
+}
 
 // switch signup/login
-  const container = document.getElementById('user-box');
-  const registerBtn = document.getElementById('register');
-  const loginBtn = document.getElementById('login');
-  
-  registerBtn.addEventListener('click', () => {
-      container.classList.add("active");
-  });
-  
-  loginBtn.addEventListener('click', () => {
-      container.classList.remove("active");
-  });
-  
+const container = document.getElementById('user-box');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
+
 // clicking login to popup the box login/sign-up
-  document.querySelector("#userBtn").addEventListener("click", function() {
-      document.querySelector(".popup").classList.add("active");
-  });
-  document.querySelector(".popup .close-btn").addEventListener("click", function() {
-      document.querySelector(".popup").classList.remove("active");
-  });
+const popup = document.querySelector(".popup");
+const userBtn = document.querySelector("#userBtn");
+const closeBtn = document.querySelector(".popup .close-btn");
+const overlayer = document.querySelector(".overlayer");
+
+userBtn.addEventListener("click", function() {
+    popup.classList.add("active");
+
+    // blur the background when logging in/signing up
+    overlayer.classList.add("pop");
+});
+closeBtn.addEventListener("click", function() {
+    popup.classList.remove("active");
+    overlayer.classList.remove("pop");
+});
